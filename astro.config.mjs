b2225bbeb2e436db507f85a87ad5743d8f48bd7f
Withered Flowers <withered-flowers@users.noreply.github.com>
2024-12-31 07:54:11 +0700

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,86 @@
+import starlight from "@astrojs/starlight";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: "Tutorial NextJS v15",
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/withastro/starlight",
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "Introduction",
+					// link: "https://www.google.com",
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{
+							label: "Step 1 - Inisialisasi Proyek",
+							link: "https://www.google.com",
+							//  slug: "guides/example"
+						},
+						{
+							label: "Step 2 - Analisa File / Folder",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 3 - Jalankan Proyek",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 4 - Membuat Routing /about",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 5 - Menambahkan anchor pada /about",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 6 - Membuat Routing /dashboard",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 7 - Membuat Component DashboardSidebar",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 8 - Membuat Routing /dashboard/jokes",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 9 - Mempopulasikan data pada /dashboard/jokes",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 10 - Membuat halaman loading untuk /dashboard/jokes",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 11 - Membuat halaman error untuk /dashboard/jokes",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 12 - Menampilkan error yang di-throw dari server",
+							link: "https://www.google.com",
+						},
+						{
+							label: "Step 13 - Membuat Dynamic Router /dashboard/jokes/[id]",
+							link: "https://www.google.com",
+						},
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Client & Server Component",
+					link: "https://www.google.com",
+				},
+				{
+					label: "Route Handler",
+					link: "https://www.google.com",
+				},
+				{
+					label: "Authentication & Middleware",
+					link: "https://www.google.com",
 				},
 			],
 		}),
